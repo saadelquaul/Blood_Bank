9f02a49f06ecfa3db404ab832ef2528decfc417d
Saad el quaul <saadelquaul@gmail.com>
2025-10-10 17:06:17 +0100

    ADDING ReceiverUgency Enum

--- src/main/java/org/blood_bank/entity/enums/ReceiverUrgency.java
+++ src/main/java/org/blood_bank/entity/enums/ReceiverUrgency.java
@@ -1,4 +1,19 @@
 package org.blood_bank.entity.enums;
 
 public enum ReceiverUrgency {
+    CRITICAL(4),
+    URGENT(3),
+    NORMAL(1);
+
+
+    private final int requiredUnits;
+
+    ReceiverUrgency(int requiredUnits) {
+        this.requiredUnits = requiredUnits;
+    }
+
+    public int getRequiredUnits() {
+        return requiredUnits;
+    }
+
 }
